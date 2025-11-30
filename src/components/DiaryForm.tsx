29f41d0b05064ab7279673eecc32e4b39d7e317a
Sanna Pitkänen <sanpitka@student.oulu.fi>
2025-11-30 17:16:16 +0200

    9.19

--- src/components/DiaryForm.tsx
+++ src/components/DiaryForm.tsx
@@ -1,19 +1,42 @@
 import { useState } from "react";
 import { NewEntry } from "../types";
 
-const DiaryForm = ({ onSubmit }: { onSubmit: (entry: NewEntry) => void }) => {
+const DiaryForm = ({ onSubmit }: { onSubmit: (entry: NewEntry) => Promise<void> }) => {
   const [date, setDate] = useState('');
   const [weather, setWeather] = useState('');
   const [visibility, setVisibility] = useState('');
   const [comment, setComment] = useState('');
+  const [error, setError] = useState<string | null>(null);
 
-  const handleSubmit = (e: React.FormEvent) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    onSubmit({ date, weather, visibility, comment });
+    setError(null);
+    try {
+      await onSubmit({ date, weather, visibility, comment });
+      setDate('');
+      setWeather('');
+      setVisibility('');
+      setComment('');
+    } catch (error: unknown) {
+  let errorMessage = '';
+  if (
+    typeof error === 'object' &&
+    error !== null &&
+    'response' in error &&
+    typeof (error as any).response?.data === 'string'
+  ) {
+    errorMessage = (error as any).response.data;
+  } else if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  setError(errorMessage);
+  setTimeout(() => { setError(null); }, 5000);
+}
   };
 
   return (
     <div>
+      {error && <div style={{ color: 'red' }}>{error}</div>}
       <h2>Add new entry</h2>
       <form onSubmit={handleSubmit}>
         <div>
