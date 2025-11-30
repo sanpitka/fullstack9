71267f228cb5e2b06269041b5add3179235cbfc7
Sanna Pitkänen <sanpitka@student.oulu.fi>
2025-11-30 21:45:47 +0200

    9.23

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Patientor frontend
 
-### Getting started
-  - To get the app running just install its dependencies with ```npm install``` and run it with ```npm run dev```.
-  - The app should work without a backend, but make sure that the request made to ```/api/ping``` made on startup is successful before continuing.
+University of Helsinki Full Stack Open course tasks 9.21-9.30
--- src/types.ts
+++ src/types.ts
@@ -19,4 +19,46 @@ export interface Patient {
   dateOfBirth?: string;
 }
 
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: {
+    startDate: string;
+    endDate: string;
+  };
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
